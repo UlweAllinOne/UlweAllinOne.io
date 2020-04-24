f449c723983ad47ffc1e6eb4ad9b970d63435d14
23395 <23395@LPTP-23395N.corp.hexaware.com>
2020-04-24 12:32:55 +0530

    UlweAllinOne

---
+++ js/contextPath.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+var getContextPath="https://4awh2d0ac8.execute-api.ap-south-1.amazonaws.com/dev/getRequestRedirection/";
+
+var postContextPath="https://4awh2d0ac8.execute-api.ap-south-1.amazonaws.com/dev/postRequestRedirection/";
