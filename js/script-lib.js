2d6a684b6cc5f384c7ecf6dbabb12358398ee87b
23395 <23395@LPTP-23395N.corp.hexaware.com>
2020-04-28 16:25:22 +0530

    ok

--- js/script-lib.js
+++ js/script-lib.js
@@ -1,28 +1,42 @@
 var map = {};
-var deliveryCharge=10;
-var disc=1;
+var deliveryCharge=0;
+var disc=0;
+var context = "https://m8gohcb5gc.execute-api.ap-south-1.amazonaws.com/dev/milk/";
 cardCount();
-function initMap(){
-	map[1]="Veg Basket,400,360,10%,Veg Basket";
-	map[2]="Veg Big Basket,500,450,10%,Veg Big Basket";
-	map[3]="1Kg Carrot,50,50,,carrot";
-	map[4]="1kg Onion,30,30,,Onion";
-	map[5]="1kg Tamato ,30,30,,Tamato";
-	map[6]="1kg Capsicum,50,50,,Capsicum";
-	map[7]="100gm Chilli,10,10,,Chilli";
-	map[8]="1kg Lasun,160,160,,Lasun";
-	map[9]="Watermelon,160,160,,watermelon";
-	map[10]="1kg Grapes,50,50,,Grapes";
-	map[11]="1 Dozen Banana,40,40,,Banana";
-	map[12]="10kg Onion,300,250,17%,Onion";
-	map[13]="1kg Flower ,50,50,,Flower ";
-	map[14]="1kg cabbage,30,30,,cabbage";
-	map[15]="1kg Bhindi,50,50,,Bhindi";
-	map[16]="1kg Loki,40,40,,Loki";
-	map[17]="1kg Kakdi,40,40,,Kakdi";
-	map[18]="1kg Beetroot,40,40,,Beetroot";
+function initMap(type){
 	
+	$.ajax({
+			  type: 'POST',
+			  url: context + "getAllMilkProducts",
+			  success: function (response) { 
+				var count =1;
+					$(response).each(function(){
+							map[count]=$(this).attr('productName')+","+$(this).attr('maxPrice')+","+$(this).attr('minPrice')+","+$(this).attr('discount')+","+$(this).attr('productDesc');
+							count++;
+					})
+					if(type == 'C'){
+						displayCardDetails();
+					}else{
+						generateProduct();
+					}
+					},
+			  error : function (response) { 						
+					$('#lodaingModal').modal('hide');
+					alert(response);
+					}
+
+			});
+	/*map[1]="Amul Cow Milk,30,27,10%,Amul Milk";
+	map[2]="Amul Full Cream Milk,60,60,,Milk";
+	map[3]="Mali Panner,200,160,20%,Panner";
+	map[4]="Kesar Shrikhand,60,60,,Shrikhand";
+	map[5]="Amul Chees,50,50,,Cheesh";
+	map[6]="Amul Butter Milk,12,12,,Butter Milk";
+	map[7]="Dahi (Curd),12,12,,Curd";
+	map[8]="White Bread,12,12,,Bread";
+	*/
 }
+
 
 function cardCount(){
 	var count= 0;
@@ -43,13 +57,14 @@ function addtoCard(id){
 }
 
 function calcuateAmt(obj,j,orgValue){
-	if($(obj).val() > 0 && $(obj).val() < 9){
-		$("#totVal"+j).html($(obj).val() * orgValue + " Rs");
+	if($(obj).val() > 0 && $(obj).val() < 10){
+		var value= $(obj).val() * orgValue;
+		$("#totVal"+j).html(value + " Rs").attr('data-val',value+','+$(obj).val());
 	}
 	mainTotal();
 	return false;
 }
-
+var finalOrderDetails="";
 function mainTotal(){
 	var sum = 0;
 	$.each($(".total"),function(i,j){
@@ -60,6 +75,7 @@ function mainTotal(){
 	$("#deliveryCharge").html(deliveryCharge + " Rs");
 	$("#disc").html(disc + " Rs")
 	$("#mainTotal").html(parseInt(sum) + parseInt(deliveryCharge) - parseInt(disc) + " Rs");
+	finalOrderDetails = sum+","+$("#mainTotal").html();
 }
 
 function removeProduct(j){
@@ -72,12 +88,17 @@ function removeProduct(j){
 	return false;
 }
 function initCart(){
-	initMap();
+	initMap('C');
+	
+}
+
+function displayCardDetails(){
+	
 	var iteams  = localStorage.getItem("card");
 	$.each(iteams.split(','),function(i,j){
 	if(j != "null" && j != ""){
 		var value = map[j].split(',');
-		var str = '<tr class="text-center" id="sectiondetails'+j+'"><td class="product-remove"><a href="#" onclick="return removeProduct('+j+')"><span class="ion-ios-close"></span></a></td><td class="image-prod"><div class="img" style="background-image:url(images/product-'+j+'.jpg);"></div></td><td class="product-name"><h3>'+value[0]+'</h3><p>'+value[4]+'</p></td><td class="price">'+value[2]+' Rs</td><td class="quantity"><div class="input-group mb-3"><input type="text" name="quantity'+j+'"  onkeyup="return calcuateAmt(this,'+j+','+value[2]+')" onchange="return calcuateAmt(this,'+j+','+value[2]+')" class="quantity form-control input-number" value="1" maxlength="1"></div></td><td id="totVal'+j+'" class="total">'+value[2]+' Rs</td></tr>';
+		var str = '<tr class="text-center" id="sectiondetails'+i+'"><td class="product-remove"><a href="#" onclick="return removeProduct('+i+')"><span class="ion-ios-close"></span></a></td><td class="image-prod"><div class="img" style="background-image:url(images/product-'+j+'.jpg);"></div></td><td class="product-name"><h3>'+value[0]+'</h3><p>'+value[4]+'</p></td><td class="price">'+value[2]+' Rs</td><td class="quantity"><div class="input-group mb-3"><input type="text" name="quantity'+i+'"  onkeyup="return calcuateAmt(this,'+i+','+value[2]+')" onchange="return calcuateAmt(this,'+i+','+value[2]+')" class="quantity form-control input-number" value="1" maxlength="1"></div></td><td id="totVal'+i+'" data-val="'+value[2]+',1" class="total">'+value[2]+' Rs</td></tr>';
 		$("#cardDetails").append(str);
 	}
 	});
@@ -85,13 +106,68 @@ function initCart(){
 }
 
 function generateProduct(){
+	
 	$.map(map, function(value,key){
 	var valuesDetails = value.split(",");
 	var disc = '';
-	if(valuesDetails[3] != ""){
+	if(valuesDetails[3] != "" && valuesDetails[3] != " "){
 		disc='<span class="status">'+valuesDetails[3] +'</span>'
 	}
 	var ourProducts = '<div class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated"><div class="product"><a href="#" class="img-prod"><img class="img-fluid" src="images/product-'+key+'.jpg" alt="UlweAllinOne">'+disc +'<div class="overlay"></div></a><div class="text py-3 pb-4 px-3 text-center"><h3><a href="#">'+valuesDetails[0]+'</a></h3><div class="d-flex"><div class="pricing123"><p class="price"><span class="mr-2 price-dc">'+valuesDetails[1]+' Rs</span><span class="price-sale">'+valuesDetails[2]+' Rs</span><span class="price-sale">&nbsp;&nbsp;</span><input type="button" onClick="return addtoCard('+key+')" class="btn btn-primary" value="Add to Cart"></p></div></div></div></div></div>';
 	$("#productDetails").append(ourProducts);
 	});
+}
+
+function checkOut(){
+	
+	var details="";
+	$(".total").each(function(){
+		details = details + $(this).attr("data-val") +"#";
+	})
+	details = details.substr(0,details.length -1);
+
+	localStorage.setItem("detailsitms",details);
+	location.href="checkout.html";	
+}
+
+function initCheckOut(){
+	var iteams  = finalOrderDetails;
+	$("#finalTotal").html(iteams.split(',')[0]);
+	$("#deliveryCharge").html(deliveryCharge);
+	$("#disc").html(disc);
+	$("#mainTotal").html(iteams.split(',')[1]);
+	
+}
+
+function placeOrder(){
+	intcheckout();
+	var iteams  = finalOrderDetails;
+	var array = {};
+	array["userName"]=$("#fname").val() + " "+$("#lname").val();
+	array["address"]=$("#sector").val()+ ", "+$("#apartment").val()+ ", "+$("#streetAddress").val()+", "+$("#city").val()+ ", "+$("#pincode").val();
+	array["emailid"]=$("#emailid").val();
+	array["mobileNo"]=$("#mobileNo").val();
+	array["devCharge"]=deliveryCharge;
+	array["discount"]=disc;
+	array["totPrice"]=iteams.split(',')[0];
+	array["finalPrice"]=iteams.split(',')[1];
+	array["orderDetails"]=localStorage.getItem("detailsitms");
+	$.ajax({
+			  type: 'POST',
+			  url: context + "saveMilkOrders",
+			  data:JSON.stringify(array),
+			  success: function (response) { 
+				alert(response)
+					},
+			  error : function (response) { 						
+					$('#lodaingModal').modal('hide');
+					alert(response);
+					}
+
+			});
+	
+	
+	
+	
+	
 }
